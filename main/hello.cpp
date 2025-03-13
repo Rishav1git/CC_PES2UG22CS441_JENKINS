21a798f6b5edb85393235c610282f5c3c4f57f4b
Rishav1git <rishu1608delhi@gmail.com>
2025-03-13 16:21:29 +0530

    made changes in hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I am Rishav Kumar Agarwal Succesfully built and run this  \n";
   cout << "Great!!";
-  
+  cout<< "ok Now it is runned automatically by jenkins";
   return 0;
 }
