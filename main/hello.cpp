60111aa5cf3aa71654ac32d0fee0baef6d36667f
Rishav1git <rishu1608delhi@gmail.com>
2025-03-13 15:40:24 +0530

    made changes in hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I am Rishav Kumar Agarwal Succesfully built and run this  \n";
+  cout << "Great!!";
   return 0;
 }
